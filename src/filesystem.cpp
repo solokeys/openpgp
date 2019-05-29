c897609aaf28951ca68e9e903add3f78327bd916
oleg <807634+merlokk@users.noreply.github.com>
2019-05-29 10:08:53 -0700

    added some test data to filesystem

--- src/filesystem.cpp
+++ src/filesystem.cpp
@@ -20,6 +20,54 @@ Util::Error ConfigFileSystem::ReadFile(AppID_t AppId, KeyID_t FileID,
 		data.set("\x61\x11\x4F\x06\xD2\x76\x00\x01\x24\x01\x50\x07OpenPGP"_bstr);
 		return Util::Error::NoError;
 
+	// General feature management. OpenPGP v 3.3.1 page 14
+	case 0x7f74:
+		data.set("\x81\x01\x28"_bstr); // Button and LED
+		return Util::Error::NoError;
+
+	// Application Related Data
+	case 0x6e:
+    /*  4F 10 D2 76 00 01 24 01 02 01 00 05 00 00 31 88 00 00 Full Application identifier (AID), ISO 7816-4
+     *  5F 52 0A 00 31 C5 73 C0 01 40 05 90 00  Historical bytes (page 38) 00 - iso format ....  05 - operational state 90 00 - ok
+     *  Discretionary data objects
+     *  73 81 B7
+     *  C0 0A 7C 00 08 00 08 00 08 00 08 00 Extended Capabilities
+     *  C1 06 01 08 00 00 20 00             Algorithm attributes signature
+     *  C2 06 01 08 00 00 20 00             Algorithm attributes decryption
+     *  C3 06 01 08 00 00 20 00             Algorithm attributes authentication
+     *  C4 07 00 20 20 20 03 00 03          PW status Bytes
+     *  C5 3C 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 Fingerprints 20b per key. order: Sig, Dec, Auth
+     *        00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+     *        00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+     *        00 00 00 00 00 00 00 00 00 00 00 00
+     *  C6 3C 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 List of CA-Fingerprints
+     *        00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+     *        00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+     *        00 00 00 00 00 00 00 00 00 00 00 00
+     *  CD 0C 00 00 00 00 00 00 00 00 00 00 00 00             List of generation dates/times of public key pairs,
+     *                                                        4 bytes each, order: Sig, Dec, Auth, seconds since Jan 1, 1970,
+    */
+		data.set("\x4F\x10\xD2\x76\x00\x01\x24\x01\x02\x01\x00\x05\x00\x00\x31\x88\x00\x00\x5F\x52\x0A\x00\x31\xC5\x73\xC0\x01\x40\x05"\
+				 "\x90\x00\x73\x81\xB7\xC0\x0A\x7C\x00\x08\x00\x08\x00\x08\x00\x08\x00\xC1\x06\x01\x08\x00\x00\x20\x00\xC2\x06\x01\x08"\
+				 "\x00\x00\x20\x00\xC3\x06\x01\x08\x00\x00\x20\x00\xC4\x07\x00\x20\x20\x20\x03\x00\x03\xC5\x3C\x00\x00\x00\x00\x00\x00"\
+				 "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"\
+				 "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xC6\x3C\x00\x00"\
+				 "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"\
+				 "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"\
+				 "\xCD\x0C\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"_bstr);
+
+		return Util::Error::NoError;
+
+	// Cardholder Related Data
+	case 0x65:
+		data.set("\x5B\x00\x5F\x2D\x02\x65\x6e\x5F\x35\x01\x39"_bstr); // 5b name, 5f2d language = en, 5f35 sex = 9(n/a)
+		return Util::Error::NoError;
+
+	// empty files
+	case 0x5e: // Login data
+		data.clear();
+		return Util::Error::NoError;
+
 	default:
 		break;
 	}
