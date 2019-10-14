f1374f969670d69c04fac36c388199f08accd8c8
merlokk <807634+merlokk@users.noreply.github.com>
2019-10-14 20:50:30 +0300

    tests refactoring

--- pytest/util.py
+++ pytest/util.py
@@ -1,7 +1,21 @@
+from tlv import *
+
+
 def get_data_object(card, tag):
     tagh = tag >> 8
     tagl = tag & 0xff
     return card.cmd_get_data(tagh, tagl)
 
+
 def check_null(data_object):
     return data_object == None or len(data_object) == 0
+
+
+def get_pk_info(pk):
+    pktlv = TLV(pk)
+    pktlv.show()
+    tag81 = pktlv.search(0x81)
+    tag82 = pktlv.search(0x82)
+    assert not (tag81 is None)
+    assert not (tag82 is None)
+    return tag81.data, tag82.data
