fe1fbf41a98f9ab03ff67eb003274c0858a88304
merlokk <807634+merlokk@users.noreply.github.com>
2019-12-23 18:13:10 +0200

    rename

--- src/main.cpp
+++ src/main.cpp
@@ -33,20 +33,20 @@ int main(int argc, char * argv[])
 	uint8_t ccidbuf[350];
     uint32_t sz;
     printf("------------------\n");
-    printf("Hello CCID/OpenPGP\n");
+    printf("OpenPGP Starting...\n");
 
     hwinit();
     printf("Init hardware ok\n");
 
     ccid_init();
-    printf("Init CCID\n");
+    printf("Init CCID ok\n");
 
     Factory::SoloFactory &factory = Factory::SoloFactory::GetSoloFactory();
     factory.Init();  // init solokey
     Applet::APDUExecutor executor = factory.GetAPDUExecutor();
     fexecutor = &executor;
 
-    printf("OpenPGP factory OK.\n");
+    printf("OpenPGP factory ok.\n");
 
 #ifdef USBIP_MODE
     printf("USBIP mode.\n");
