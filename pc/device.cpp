110743c744172427cf1d92bdb30ef6d4b01eea0a
merlokk <807634+merlokk@users.noreply.github.com>
2019-12-26 17:04:59 +0200

    spiffs sketch

--- pc/device.cpp
+++ pc/device.cpp
@@ -19,54 +19,83 @@
 #ifdef SPIFFS_MODE
 #include <spiffs.h>
 static spiffs fs;
+static uint8_t fsbuf[2048*10] = {0};
+static char *SpiffsFileName = (char *)"filesystem.spiffs";
 #endif
 
 #define LOG_PAGE_SIZE 64
+
+bool ifileexist(char* name);
+int ireadfile(char* name, uint8_t * buf, size_t max_size, size_t *size);
 
 static u8_t spiffs_work_buf[LOG_PAGE_SIZE * 2];
 static u8_t spiffs_fds[32 * 4];
 static u8_t spiffs_cache_buf[(LOG_PAGE_SIZE + 32) * 4];
 
 static s32_t hw_spiffs_read(u32_t addr, u32_t size, u8_t *dst) {
-  //my_spi_read(addr, size, dst);
-  return SPIFFS_OK;
+	printf("~~read a=%x s=%d\n", addr, size);
+	memcpy(dst, fsbuf + addr, size);
+	return SPIFFS_OK;
 }
 
 static s32_t hw_spiffs_write(u32_t addr, u32_t size, u8_t *src) {
-  //my_spi_write(addr, size, src);
-  return SPIFFS_OK;
+	printf("~~write\n");
+	memcpy(fsbuf + addr, src, size);
+	return SPIFFS_OK;
 }
 
 static s32_t hw_spiffs_erase(u32_t addr, u32_t size) {
-  //my_spi_erase(addr, size);
-  return SPIFFS_OK;
+	printf("~~erase\n");
+	memset(fsbuf + addr, 0xff, size);
+	return SPIFFS_OK;
 }
 
 void hw_spiffs_mount() {
-  spiffs_config cfg;
-  cfg.phys_size = 10*2048; // use all spi flash
-  cfg.phys_addr = 0;       // start spiffs at start of spi flash
-  cfg.phys_erase_block = 2048; // according to datasheet
-  cfg.log_block_size = 2048;   // let us not complicate things
-  cfg.log_page_size = LOG_PAGE_SIZE; // as we said
+	spiffs_config cfg;
+	cfg.phys_size = 10*2048; // use all spi flash
+	cfg.phys_addr = 0;       // start spiffs at start of spi flash
+	cfg.phys_erase_block = 2048; // according to datasheet
+	cfg.log_block_size = 2048;   // let us not complicate things
+	cfg.log_page_size = LOG_PAGE_SIZE; // as we said
 
-  cfg.hal_read_f = hw_spiffs_read;
-  cfg.hal_write_f = hw_spiffs_write;
-  cfg.hal_erase_f = hw_spiffs_erase;
+	cfg.hal_read_f = hw_spiffs_read;
+	cfg.hal_write_f = hw_spiffs_write;
+	cfg.hal_erase_f = hw_spiffs_erase;
 
-  int res = SPIFFS_mount(&fs,
-    &cfg,
-    spiffs_work_buf,
-    spiffs_fds,
-    sizeof(spiffs_fds),
-    spiffs_cache_buf,
-    sizeof(spiffs_cache_buf),
-    0);
-  printf("mount res: %i\n", res);
+	int res = SPIFFS_mount(&fs,
+		&cfg,
+		spiffs_work_buf,
+		spiffs_fds,
+		sizeof(spiffs_fds),
+		spiffs_cache_buf,
+		sizeof(spiffs_cache_buf),
+		0);
+	printf("mount res: %i\n", res);
+
+	if (res || !SPIFFS_mounted(&fs)) {
+		res = SPIFFS_format(&fs);
+		printf("format res: %i\n", res);
+	}
+
+	uint32_t total = 0;
+	uint32_t used = 0;
+	if (SPIFFS_info(&fs, &total, &used))
+		printf("Mounted OK. Memory total: %d used: %d", total, used);
 }
 
 int hwinit() {
+#ifdef SPIFFS_MODE
+	memset(fsbuf, 0xff, sizeof(fsbuf));
+
+	if (ifileexist(SpiffsFileName)) {
+		size_t size = 0;
+		ireadfile(SpiffsFileName, fsbuf, sizeof(fsbuf), &size);
+		if (size != sizeof(fsbuf))
+			memset(fsbuf, 0xff, sizeof(fsbuf));
+	}
+
 	hw_spiffs_mount();
+#endif
 
 	return 0;
 }
@@ -172,8 +201,7 @@ void make_work_directory(char* dir) {
 	}
 }
 
-bool fileexist(char* name) {
-
+bool ifileexist(char* name) {
 	char fname[100] = {0};
 	char dir[] = "./data/";
 	make_work_directory(dir);
@@ -189,7 +217,29 @@ bool fileexist(char* name) {
 	return true;
 }
 
-int readfile(char* name, uint8_t * buf, size_t max_size, size_t *size) {
+bool sfileexist(char* name) {
+	spiffs_DIR d;
+	struct spiffs_dirent e;
+	struct spiffs_dirent *pe = &e;
+
+	SPIFFS_opendir(&fs, "/", &d);
+	while ((pe = SPIFFS_readdir(&d, pe))) {
+		if (0 == strcmp(name, (char *)pe->name)) {
+			return true;
+		}
+	}
+	return false;
+}
+
+bool fileexist(char* name) {
+#ifdef SPIFFS_MODE
+	return sfileexist(name);
+#else
+	return ifileexist(name);
+#endif
+}
+
+int ireadfile(char* name, uint8_t * buf, size_t max_size, size_t *size) {
 
 	char fname[100] = {0};
 	char dir[] = "./data/";
@@ -213,7 +263,32 @@ int readfile(char* name, uint8_t * buf, size_t max_size, size_t *size) {
 	return 0;
 }
 
-int writefile(char* name, uint8_t * buf, size_t size) {
+int sreadfile(char* name, uint8_t * buf, size_t max_size, size_t *size) {
+	*size = 0;
+
+	spiffs_file fd = SPIFFS_open(&fs, name, SPIFFS_RDWR, 0);
+	if (fd < 0)
+		return fd;
+
+	int res = SPIFFS_read(&fs, fd, buf, max_size);
+
+	*size = res;
+	int cres = SPIFFS_close(&fs, fd) < 0;
+	if (cres < 0)
+		return cres;
+
+	return (res >= 0) ? 0 : res;
+}
+
+bool readfile(char* name, uint8_t * buf, size_t max_size, size_t *size) {
+#ifdef SPIFFS_MODE
+	return sreadfile(name, buf, max_size, size);
+#else
+	return ireadfile(name, buf, max_size, size);
+#endif
+}
+
+int iwritefile(char* name, uint8_t * buf, size_t size) {
 	char fname[100] = {0};
 	char dir[] = "./data/";
 	make_work_directory(dir);
@@ -235,7 +310,29 @@ int writefile(char* name, uint8_t * buf, size_t size) {
 	return 0;
 }
 
-int deletefile(char* name) {
+int swritefile(char* name, uint8_t * buf, size_t size) {
+	spiffs_file fd = SPIFFS_open(&fs, name, SPIFFS_CREAT | SPIFFS_TRUNC | SPIFFS_RDWR, 0);
+	if (fd < 0)
+		return fd;
+
+	int res = SPIFFS_write(&fs, fd, buf, size);
+
+	int cres = SPIFFS_close(&fs, fd) < 0;
+	if (cres < 0)
+		return cres;
+
+	return (res >= 0) ? 0 : res;
+}
+
+int writefile(char* name, uint8_t * buf, size_t size) {
+#ifdef SPIFFS_MODE
+	return swritefile(name, buf, size);
+#else
+	return iwritefile(name, buf, size);
+#endif
+}
+
+int ideletefile(char* name) {
 	char fname[100] = {0};
 	char dir[] = "./data/";
 	make_work_directory(dir);
@@ -247,8 +344,15 @@ int deletefile(char* name) {
 	remove(fname);
 	return 0;
 }
+int deletefile(char* name) {
+#ifdef SPIFFS_MODE
+	return SPIFFS_remove(&fs, name);
+#else
+	return ideletefile(name);
+#endif
+}
 
-int deletefiles(char* name) {
+int ideletefiles(char* name) {
 	char fname[100] = {0};
 	char dir[] = "./data/";
 	make_work_directory(dir);
@@ -272,6 +376,66 @@ int deletefiles(char* name) {
     strcat(fname, name);
 
     return 0;
+}
+
+int sprintfs() {
+	spiffs_DIR d;
+	struct spiffs_dirent e;
+	struct spiffs_dirent *pe = &e;
+	int res;
+
+	uint32_t total = 0;
+	uint32_t used = 0;
+	SPIFFS_info(&fs, &total, &used);
+	printf("Memory total: %d used: %d", total, used);
+
+	SPIFFS_opendir(&fs, "/", &d);
+	while ((pe = SPIFFS_readdir(&d, pe))) {
+		printf("  [%d] %s", pe->size, pe->name);
+	}
+	SPIFFS_closedir(&d);
+	return 0;
+}
+
+int sdeletefiles(char* name) {
+	spiffs_DIR d;
+	struct spiffs_dirent e;
+	struct spiffs_dirent *pe = &e;
+	int res;
+
+	sprintfs();
+
+	SPIFFS_opendir(&fs, "/", &d);
+	while ((pe = SPIFFS_readdir(&d, pe))) {
+		if ((fnmatch(name, (char *)pe->name, 0)) == 0) {
+			fd = SPIFFS_open_by_dirent(&fs, pe, SPIFFS_RDWR, 0);
+			if (fd < 0) {
+				printf("errno %i\n", SPIFFS_errno(&fs));
+				return SPIFFS_errno(&fs);
+			}
+			res = SPIFFS_fremove(&fs, fd);
+			if (res < 0) {
+				printf("errno %i\n", SPIFFS_errno(&fs));
+				return SPIFFS_errno(&fs);
+			}
+			res = SPIFFS_close(&fs, fd);
+			if (res < 0) {
+				printf("errno %i\n", SPIFFS_errno(&fs));
+				return SPIFFS_errno(&fs);
+			}
+		}
+	}
+	SPIFFS_closedir(&d);
+	exit(100);
+	return 0;
+}
+
+int deletefiles(char* name) {
+#ifdef SPIFFS_MODE
+	return sdeletefiles(name);
+#else
+	return ideletefiles(name);
+#endif
 }
 
 int hwreboot() {
