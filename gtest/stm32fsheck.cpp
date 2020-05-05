cd7711e083a049f5e93db5c041f306a9cc27b716
merlokk <807634+merlokk@users.noreply.github.com>
2020-05-05 19:22:53 +0300

    add define for sector size

--- gtest/stm32fsheck.cpp
+++ gtest/stm32fsheck.cpp
@@ -5,15 +5,17 @@
 #include <array>
 #include "../libs/stm32fs/stm32fs.h"
 
+#define SECTOR_SIZE 2048
+
 TEST(stm32fsTest, Create) {
-    uint8_t vmem[2048 * 10];
+    uint8_t vmem[SECTOR_SIZE * 10];
     std::memset(vmem, 0xff, sizeof(vmem));
     
     Stm32fsConfig_t cfg;
     cfg.BaseBlockAddress = 0;
-    cfg.SectorSize = 2048;
+    cfg.SectorSize = SECTOR_SIZE;
     cfg.Blocks = {{{0,1}, {2,3,4}}};
-    cfg.fnEraseFlashBlock = [&vmem](uint8_t blockNo){std::memset(&vmem[2048 * blockNo], 0xff, 2048);return true;};
+    cfg.fnEraseFlashBlock = [&vmem](uint8_t blockNo){std::memset(&vmem[SECTOR_SIZE * blockNo], 0xff, SECTOR_SIZE);return true;};
     cfg.fnWriteFlash = [&vmem](uint32_t, uint8_t *data, size_t len){return true;};
     cfg.fnReadFlash = [&vmem](uint32_t, uint8_t *data, size_t *len){return true;};
     
