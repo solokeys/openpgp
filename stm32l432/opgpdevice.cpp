4adc93cd774c0d89274fcedaf7f3228d523c524c
merlokk <807634+merlokk@users.noreply.github.com>
2020-05-23 16:57:18 +0300

    delete spiffs

--- stm32l432/opgpdevice.cpp
+++ stm32l432/opgpdevice.cpp
@@ -20,99 +20,22 @@
 #include "util.h"
 
 #include "stm32fs.h"
-#include <spiffs.h>
-static spiffs fs;
 
-#define LOG_PAGE_SIZE 64
-// 2048 for this CPU
-#define BLOCK_SIZE PAGE_SIZE
-#define TOTAL_FS_SIZE OPENPGP_NUM_PAGES*BLOCK_SIZE
+static Stm32fs fs;
 
 void sprintfs();
-
-PUT_TO_SRAM2 static u8_t spiffs_work_buf[LOG_PAGE_SIZE * 2];
-PUT_TO_SRAM2 static u8_t spiffs_fds[32 * 4];
-PUT_TO_SRAM2 static u8_t spiffs_cache_buf[(LOG_PAGE_SIZE + 32) * 4];
-
-static s32_t hw_spiffs_read(u32_t addr, u32_t size, u8_t *dst) {
-    if (addr < OPENPGP_START_PAGE_ADDR || addr + size - 1 > OPENPGP_END_PAGE_ADDR) {
-        printf_device("spiffs read address %x [%d] error\n", addr, size);
-        return SPIFFS_ERR_INTERNAL;
-    }
-    
-    memmove(dst, (u8_t *)addr, size);
-	return SPIFFS_OK;
-}
-
-static s32_t hw_spiffs_write(u32_t addr, u32_t size, u8_t *src) {
-    if (addr < OPENPGP_START_PAGE_ADDR || addr + size - 1 > OPENPGP_END_PAGE_ADDR) {
-        printf_device("spiffs write address %x [%d] error\n", addr, size);
-        return SPIFFS_ERR_INTERNAL;
-    }
-    
-    flash_write_ex(addr, src, size);
-	return SPIFFS_OK;
-}
-
-static s32_t hw_spiffs_erase(u32_t addr, u32_t size) {
-    for(u32_t x = 0; x < size; x += 2048) {
-        uint8_t page = OPENPGP_START_PAGE + (addr - OPENPGP_START_PAGE_ADDR) / BLOCK_SIZE;
-        if (page < OPENPGP_START_PAGE || page > OPENPGP_END_PAGE) {
-            printf_device("spiffs erase address %x error\n", addr);
-            return SPIFFS_ERR_INTERNAL;
-        }
-        flash_erase_page(page + x / 2048);
-    }
-	return SPIFFS_OK;
-}
-
-void hw_spiffs_mount() {
-	spiffs_config cfg;
-	cfg.phys_size = TOTAL_FS_SIZE;           // use size as in `memory_layout.h`
-	cfg.phys_addr = OPENPGP_START_PAGE_ADDR; // start memory area for OpenPGP
-	cfg.phys_erase_block = BLOCK_SIZE;       // block size as in CPU
-	cfg.log_block_size = BLOCK_SIZE;         // let us not complicate things
-	cfg.log_page_size = LOG_PAGE_SIZE;       // page size for filesystem
-
-	cfg.hal_read_f = hw_spiffs_read;
-	cfg.hal_write_f = hw_spiffs_write;
-	cfg.hal_erase_f = hw_spiffs_erase;
-    
-	int res = SPIFFS_mount(&fs,
-		&cfg,
-		spiffs_work_buf,
-		spiffs_fds,
-		sizeof(spiffs_fds),
-		spiffs_cache_buf,
-		sizeof(spiffs_cache_buf),
-		0);
-	printf_device("mount res: %i\n", res);
-
-	if (res || !SPIFFS_mounted(&fs)) {
-		res = SPIFFS_format(&fs);
-		printf_device("format res: %i\n", res);
-	}
-	printf_device("SPIFFS mount OK. rawsize: %db\n", TOTAL_FS_SIZE);
-	sprintfs();
-    
-    // slow but needs)
-    res = SPIFFS_check(&fs);
-    if (res == 0)
-        printf_device("SPIFFS check OK.\n");
-    else
-        printf_device("SPIFFS check error: %d.\n", res);
-}
 
 void hw_stm32fs_init() {
     static Stm32fsConfig_t cfg;
     cfg.BaseBlockAddress = 0;
-    cfg.SectorSize = BLOCK_SIZE;
+    cfg.SectorSize = PAGE_SIZE;
     cfg.Blocks = {{{OPENPGP_START_PAGE}, {OPENPGP_START_PAGE + 1, OPENPGP_START_PAGE + 2, OPENPGP_START_PAGE + 3}}};
     cfg.fnEraseFlashBlock = [](uint8_t blockNo){flash_erase_page(blockNo);return true;};
     cfg.fnWriteFlash = [](uint32_t address, uint8_t *data, size_t len){flash_write_ex(address, data, len);return true;};
-    cfg.fnReadFlash = [](uint32_t address, uint8_t *data, size_t len){memcpy(data, (u8_t *)address, len);return true;};
+    cfg.fnReadFlash = [](uint32_t address, uint8_t *data, size_t len){memcpy(data, (uint8_t *)address, len);return true;};
 
-    Stm32fs fs{cfg};
+    fs = Stm32fs(cfg);
+    // TODO: check if it needs to call optimize...
     if (fs.isValid())
         printf_device("stm32fs [%d] OK.\n", fs.GetCurrentFsBlockSerial());
     else
@@ -120,7 +43,6 @@ void hw_stm32fs_init() {
 }
 
 int hwinit() {
-	hw_spiffs_mount();
 device_led(COLOR_BLUE);
     hw_stm32fs_init();
 device_led(COLOR_GREEN);
@@ -129,118 +51,35 @@ device_led(COLOR_GREEN);
 }
 
 bool fileexist(char* name) {
-	spiffs_DIR d;
-	struct spiffs_dirent e;
-	struct spiffs_dirent *pe = &e;
-
-	SPIFFS_opendir(&fs, "/", &d);
-	while ((pe = SPIFFS_readdir(&d, pe))) {
-		if (0 == strcmp(name, (char *)pe->name)) {
-			return true;
-		}
-	}
-	return false;
+    return fs.FileExist(name);
 }
 
 int readfile(char* name, uint8_t * buf, size_t max_size, size_t *size) {
-	*size = 0;
-
-	spiffs_file fd = SPIFFS_open(&fs, name, SPIFFS_RDWR, 0);
-	if (fd < 0)
-		return fd;
-
-	int res = SPIFFS_read(&fs, fd, buf, max_size);
-
-	*size = res;
-	int cres = SPIFFS_close(&fs, fd) < 0;
-	if (cres < 0)
-		return cres;
-
-	return (res >= 0) ? 0 : res;
+    return fs.ReadFile(name, buf, size, max_size);
 }
 
 int writefile(char* name, uint8_t * buf, size_t size) {
-	spiffs_file fd = SPIFFS_open(&fs, name, SPIFFS_CREAT | SPIFFS_TRUNC | SPIFFS_RDWR, 0);
-	if (fd < 0) {
-        printf("spiffs write file descriptor error `%s` %d\n", name, fd);
-		return fd;
-    }
-
-	int res = SPIFFS_write(&fs, fd, buf, size);
-
-    if (res < 0)
-        printf("spiffs write file error `%s` %d\n", name, res);
-
-	int cres = SPIFFS_close(&fs, fd) < 0;
-	if (cres < 0) {
-        printf("spiffs write file close error `%s` %d\n", name, cres);
-		return cres;
-    }
-
-	return (res >= 0) ? 0 : res;
+    return fs.WriteFile(name, buf, size);
 }
 
 int deletefile(char* name) {
-	return SPIFFS_remove(&fs, name);
+    return fs.DeleteFile(name);
 }
 
 void sprintfs() {
-	spiffs_DIR d;
-	struct spiffs_dirent e;
-	struct spiffs_dirent *pe = &e;
+    printf_device("Memory total: %d free: %d free descriptors: %d\n",
+                  fs.GetSize(), fs.GetFreeMemory(), fs.GetFreeFileDescriptors());
 
-	u32_t total = 0;
-	u32_t used = 0;
-	SPIFFS_info(&fs, &total, &used);
-	printf_device("Memory total: %d used: %d\n", total, used);
-
-	SPIFFS_opendir(&fs, "/", &d);
+    /*SPIFFS_opendir(&fs, "/", &d);
 	while ((pe = SPIFFS_readdir(&d, pe))) {
 		printf_device("  [%4d] %s\n", pe->size, pe->name);
 	}
-	SPIFFS_closedir(&d);
+    SPIFFS_closedir(&d);*/
 	return;
 }
 
-bool fnmatch(char *pattern, char*name){
-    if (strcmp(pattern, name) == 0)
-        return true;
-
-    if (strcmp(pattern, "*") == 0)
-        return true;
-    
-    size_t xlen = MIN(strlen(pattern), strlen(name));
-    for (size_t i = 0; i < xlen; i++) {
-        if (pattern[i] == '*')
-            return true;
-        if (pattern[i] != '?' &&
-            pattern[i] != name[i])
-            return false;
-    }
-    
-    // exact match with length
-    return (strlen(pattern) == strlen(name));
-}
-
-int deletefiles(char* name) {
-	spiffs_DIR d;
-	struct spiffs_dirent e;
-	struct spiffs_dirent *pe = &e;
-	int res;
-
-	SPIFFS_opendir(&fs, "/", &d);
-	while ((pe = SPIFFS_readdir(&d, pe))) {
-		if (fnmatch(name, (char *)pe->name)) {
-			spiffs_file fd = SPIFFS_open_by_dirent(&fs, pe, SPIFFS_RDWR, 0);
-			if (fd < 0)
-				return SPIFFS_errno(&fs);
-			res = SPIFFS_fremove(&fs, fd);
-			if (res < 0)
-				return SPIFFS_errno(&fs);
-		}
-	}
-	SPIFFS_closedir(&d);
-	return 0;
+int deletefiles(char* name_filter) {
+    return fs.DeleteFiles(name_filter);
 }
 
 int hw_reset_fs_and_reboot(bool reboot) {
