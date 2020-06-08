cac671117977d13d8de2d464ea9110df066e7dc4
merlokk <807634+merlokk@users.noreply.github.com>
2020-06-08 23:22:44 +0300

    test get_challenge command

---
+++ pytest/test_037_challenge.py
@@ -0,0 +1,25 @@
+"""
+test_037_challenge.py - test setting rsa 4096
+
+Copyright (C) 2020  SoloKeys
+Author: Oleg Moiseenko (merlokk)
+
+"""
+
+from skip_gnuk_only_tests import *
+
+from card_const import *
+from constants_for_test import *
+from openpgp_card import *
+
+
+def test_challenge(card):
+    rndval = card.cmd_get_challenge(10)
+    assert len(rndval) == 10
+    assert rndval[0] != rndval[1] and rndval[1] != rndval[2]
+    assert rndval[3] != rndval[4] and rndval[4] != rndval[5]
+
+def test_challenge_long(card):
+    rndval = card.cmd_get_challenge(255)
+    assert len(rndval) == 255
+
