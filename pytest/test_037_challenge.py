34f4efb5ee266d8524404931276f414d347806cc
merlokk <807634+merlokk@users.noreply.github.com>
2020-06-09 14:29:16 +0300

    fix challenge test

--- pytest/test_037_challenge.py
+++ pytest/test_037_challenge.py
@@ -16,8 +16,8 @@ from openpgp_card import *
 def test_challenge(card):
     rndval = card.cmd_get_challenge(10)
     assert len(rndval) == 10
-    assert rndval[0] != rndval[1] and rndval[1] != rndval[2]
-    assert rndval[3] != rndval[4] and rndval[4] != rndval[5]
+    assert rndval[0] != rndval[1] and rndval[1] != rndval[2] and \
+           rndval[3] != rndval[4] and rndval[4] != rndval[5]
 
 def test_challenge_long(card):
     rndval = card.cmd_get_challenge(255)
